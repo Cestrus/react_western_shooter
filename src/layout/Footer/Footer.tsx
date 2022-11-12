5fd4a4847419ff4b0555db32e6e0304011afc2c3
Aleksey Savchenko <wasxela@gmail.com>
2022-11-12 23:02:27 +0200

    added corners

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import CornerMain from '../../components/CornerMain/CornerMain';
 
 import styles from './Footer.module.css';
 
 const Footer: React.FC = () => {
-  return <footer className={styles.footer}>FOOTER</footer>;
+  return (
+    <footer className={styles.footer}>
+      <CornerMain position='bottom-left' />
+
+      <CornerMain position='bottom-right' />
+    </footer>
+  );
 };
 
 export default Footer;
