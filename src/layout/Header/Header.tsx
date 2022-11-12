a2fba0b75239f878d98a7b546975fe695bd15177
Aleksey Savchenko <wasxela@gmail.com>
2022-11-12 22:39:30 +0200

    created Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import CornerMain from '../../components/CornerMain/CornerMain';
+import TitleMain from '../../components/TitleMain/TitleMain';
 
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
-  return <header className={styles.header}>HEADER</header>;
+  return (
+    <header className={styles.header}>
+      <CornerMain position='top-left' />
+      <TitleMain>WESTERN SHOT</TitleMain>
+      <CornerMain position='top-right' />
+    </header>
+  );
 };
 
 export default Header;
