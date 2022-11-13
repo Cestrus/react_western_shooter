3e900885d155547fee91ef3c3d645fc8394a568b
Aleksey Savchenko <wasxela@gmail.com>
2022-11-13 16:56:26 +0200

    change counter styles

--- src/components/CounterMoney/CounterMoney.tsx
+++ src/components/CounterMoney/CounterMoney.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
+import cn from 'classnames';
 
 import styles from './CounterMoney.module.css';
 import { ICountMoneyProps } from './CounterMoney.prop';
 
-const CounterMoney: React.FC<ICountMoneyProps> = () => {
+const CounterMoney: React.FC<ICountMoneyProps> = ({ className }) => {
   const moneyValue = useSelector((state: RootState) => state.money.moneyValue);
 
   return (
-    <div className={styles.counter}>
+    <div className={cn(styles.counter, className)}>
       <h2 className={styles.text}>${moneyValue}</h2>
     </div>
   );
