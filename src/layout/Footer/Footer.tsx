0adce009e49d50ec71212076a032460dbe6b54b9
Aleksey Savchenko <wasxela@gmail.com>
2022-11-13 16:57:39 +0200

    added counter

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CornerMain from '../../components/CornerMain/CornerMain';
+import CounterMoney from '../../components/CounterMoney/CounterMoney';
 
 import styles from './Footer.module.css';
 
@@ -7,7 +8,7 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <CornerMain position='bottom-left' />
-
+      <CounterMoney className={styles.counter} />
       <CornerMain position='bottom-right' />
     </footer>
   );
