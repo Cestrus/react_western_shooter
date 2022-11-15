2fb1e718f6fb898d4adf061373f442860b40d82a
Aleksey Savchenko <wasxela@gmail.com>
2022-11-15 10:00:00 +0200

    fix logic bullets container

--- src/components/BulletsContainer/BulletsContainer.tsx
+++ src/components/BulletsContainer/BulletsContainer.tsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { RootState } from '../../store/store';
 import styles from './BulletsContainer.module.css';
 import { IBulletsContainerProps } from './BulletsContainer.prop';
-import { BULLETS_VALUE, BULLETS_IN_GUN } from '../../utils/constants';
+import { BULLETS_IN_GUN } from '../../utils/constants';
 import { setGameIsOver } from '../../store/playerSlice';
+import { reloadGun } from '../../store/bulletsSlice';
 
 export const BulletsContainer: React.FC<IBulletsContainerProps> = () => {
   const isGunEmpty = useSelector((state: RootState) => state.bullets.isGunEmpty);
+  const bulletsValue = useSelector((state: RootState) => state.bullets.bulletsValue);
   const dispatch = useDispatch();
-  const [bullets, setBullets] = useState<number>(BULLETS_VALUE - BULLETS_IN_GUN);
 
-  const tanksBulletsArr = new Array(bullets / BULLETS_IN_GUN).fill(<></>);
+  const tanksBulletsArr = new Array(bulletsValue / BULLETS_IN_GUN).fill(<></>);
 
   useEffect(() => {
     if (!isGunEmpty) {
       return;
     }
-    const bulletsRemain = bullets - BULLETS_IN_GUN;
-    bulletsRemain >= 0 ? setBullets(bulletsRemain) : dispatch(setGameIsOver);
+    bulletsValue - BULLETS_IN_GUN >= 0 ? dispatch(reloadGun) : dispatch(setGameIsOver);
   }, [isGunEmpty]);
 
   const renderBulletsTank = (idxTank: number): JSX.Element => {
