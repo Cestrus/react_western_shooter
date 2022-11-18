a55f102edea44a63b7235035fd2a0ed358d4149d
Aleksey Savchenko <wasxela@gmail.com>
2022-11-18 11:47:16 +0200

    created authorizedComponent

--- src/components/Modal/ModalOverlay/ModalOverlay.tsx
+++ src/components/Modal/ModalOverlay/ModalOverlay.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ModalType } from '../../../types/globalTypes';
+import { AuthorizedWindow } from '../../AuthorizedWindow/AuthorizedWindow';
 
 import styles from './ModalOverlay.module.css';
 import { IModalOverlay } from './ModalOverlay.prop';
@@ -7,7 +8,7 @@ import { IModalOverlay } from './ModalOverlay.prop';
 const ModalOverlay: React.FC<IModalOverlay> = ({ modalType }) => {
   return (
     <div className={styles.container}>
-      {/* {modalType === ModalType.AUTHORIZATION} */}
+      {modalType === ModalType.AUTHORIZATION && <AuthorizedWindow />}
       {/* {modalType === ModalType.GAMEOVER} */}
     </div>
   );
