52e096fc143ada03bd8a510ec972a04cd452f7c6
Aleksey Savchenko <wasxela@gmail.com>
2022-11-23 14:04:03 +0200

    created second side shooterPanel

--- src/components/ShooterPanel/ShooterPanel.tsx
+++ src/components/ShooterPanel/ShooterPanel.tsx
@@ -7,13 +7,16 @@ export const ShooterPanel: React.FC<IShooterPanelProps> = () => {
   return (
     <div className={styles.container}>
       <div className={styles.plank}>
-        <div className={styles.paper}>
-          <h4 className={styles.title}>WANTED</h4>
-          <div className={styles.shooter}>
-            <div className={styles.bloodZone}></div>
+        {true && <div className={styles.guns}></div>}
+        {false && (
+          <div className={styles.paper}>
+            <h4 className={styles.title}>WANTED</h4>
+            <div className={styles.shooter}>
+              <div className={styles.bloodZone}></div>
+            </div>
+            <h5 className={styles.price}>${100}</h5>
           </div>
-          <h5 className={styles.price}>${100}</h5>
-        </div>
+        )}
       </div>
     </div>
   );
